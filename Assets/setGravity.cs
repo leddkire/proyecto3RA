ef618cc940957d23d9ea9a311276ddb6f5318811
leddkire <leddkire@gmail.com>
2015-03-10 12:31:23 -0430

    First commit

---
+++ Assets/setGravity.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setGravity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.collider.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		gameObject.collider.enabled = true;
+	}
+}
